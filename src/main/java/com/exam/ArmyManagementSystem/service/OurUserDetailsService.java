36a07e21f4082f94bc9abe48189b71c5d3348d08
HARERIMANAGaspard <168564696+HARERIMANAGaspard@users.noreply.github.com>
2024-12-15 17:00:22 +0200

    Update OurUserDetailsService.java

--- src/main/java/com/exam/ArmyManagementSystem/service/OurUserDetailsService.java
+++ src/main/java/com/exam/ArmyManagementSystem/service/OurUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.exam.ArmyManagementSystem.service;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
