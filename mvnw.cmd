d451e5ffcc1f58c8b808ba582288eca7fb680e80
HARERIMANAGaspard <168564696+HARERIMANAGaspard@users.noreply.github.com>
2024-12-15 16:57:40 +0200

    Update mvnw.cmd

--- mvnw.cmd
+++ mvnw.cmd
@@ -77,9 +77,11 @@ echo location of your Java installation. >&2
 echo.
 goto error
 
+
 @REM ==== END VALIDATION ====
 
 :init
+
 
 @REM Find the project base dir, i.e. the directory that contains the folder ".mvn".
 @REM Fallback to current working directory if not found.
